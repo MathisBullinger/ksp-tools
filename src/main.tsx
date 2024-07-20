6dd4def1e17336fef4436eea5f0d5beae58fc9ba
Mathis Bullinger <mathis@bullinger.dev>
2024-07-20 11:46:16 +0200

    initial setup

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import {
+  LocationProvider,
+  Router,
+  Route,
+  hydrate,
+  prerender as ssr,
+} from "preact-iso";
+
+import PageRelayNet from "./pages/relaynet";
+import { NotFound } from "./pages/_404.jsx";
+import "./style.css";
+
+export function App() {
+  return (
+    <LocationProvider>
+      <Router>
+        <Route path="/" component={PageRelayNet} />
+        <Route default component={NotFound} />
+      </Router>
+    </LocationProvider>
+  );
+}
+
+if (typeof window !== "undefined") {
+  hydrate(<App />, document.getElementById("app"));
+}
+
+export async function prerender(data) {
+  return await ssr(<App {...data} />);
+}
---
+++ src/pages/relaynet/index.tsx
@@ -0,0 +1,5 @@
+const RelayNet = () => {
+  return <div>relay network planner</div>;
+};
+
+export default RelayNet;
